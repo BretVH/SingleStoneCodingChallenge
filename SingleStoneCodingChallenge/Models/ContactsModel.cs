8b7d2e2c1b326aa33b5d41f84e89b8a85ec4d77b
Bret A. Van Hof <bretvh@hotmail.com>
2020-06-22 14:10:06 -0400

    New Web API Project.
    initial commit with model

---
+++ SingleStoneCodingChallenge/Models/ContactsModel.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace SingleStoneCodingChallenge.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class ContactsModel
+    {
+        public Name Name { get; set; }
+        public Address Address { get; set; }
+        public Phone[] Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+    [ExcludeFromCodeCoverage]
+    public class Name
+    {
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Type { get; set; }
+    }
+}
